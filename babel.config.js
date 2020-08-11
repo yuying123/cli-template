0f88e165cf656189b3c9e70dba6fa806b0737338
yuying <yuying600109@pwrd.com>
2020-08-11 19:56:13 +0800

    yy

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  // sourceType: 'unambiguous',
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  // "plugins": ["syntax-dynamic-import","transform-es2015-modules-commonjs",]
 }
---
+++ config/config.js
@@ -0,0 +1,15 @@
+/*
+ * @Description: modify content
+ * @Author: Fly
+ * @Date: 2020-04-01 11:37:24
+ * @LastEditTime: 2020-04-28 11:20:36
+ * @LastEditors: Fly
+ */
+module.exports = {
+    // allHistoryHostName: '__ALLHISTORY_HOSTNAME__',
+    "__ALLHISTORY_HOSTNAME__":'//10.4.40.168:9877',
+    // "__ALLHISTORY_HOSTNAME__":'//ahadmintest.sys.wanmei.net'
+    // allHistoryMapHostName: '__ALLHISTORY_MAP_HOSTNAME__',
+    // allHistoryMapPgcHostName: '__ALLHISTORY_MAPPGC_HOSTNAME__',
+    // allHistoryMapPgcWsHostName: '__ALLHISTORY_MAPPGC_WS_HOSTNAME__',
+}
